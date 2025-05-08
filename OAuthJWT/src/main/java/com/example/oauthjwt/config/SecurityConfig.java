765d0f687586080cc0a5165c1f1701062f96b78f
goosebbeoms <seo980916@gmail.com>
2025-05-08 23:47:39 +0900

    초기 SecurityConfig 클래스 작성

---
+++ OAuthJWT/src/main/java/com/example/oauthjwt/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.oauthjwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(auth -> auth.disable());
+
+        http
+                .formLogin(auth -> auth.disable());
+
+        http
+                .httpBasic(auth -> auth.disable());
+
+        http
+                .oauth2Login(Customizer.withDefaults());
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
